f7b2ff77f718e1f397c13986cb6cc14bb8087647
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-06-02 17:40:13 +0200

    Create prompt implemented.

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Form from '@components/Form';
+
+const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const res = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+          userId: session?.user.id,
+        }),
+      });
+      if (response.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      throw Error(error.message);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
+
+export default CreatePrompt;
